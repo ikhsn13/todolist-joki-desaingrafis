044792160047c42fa8eee3318ef6776d6d5986ea
ikhsn13 <mikhsnit@gmail.com>
2026-06-16 21:26:08 +0700

    tes4

--- src/lib/db/turso.ts
+++ src/lib/db/turso.ts
@@ -2,14 +2,12 @@ import { createClient } from '@libsql/client';
 import { env } from '$env/dynamic/private';
 
 export function getDb() {
-  console.log('DATABASE_URL:', env.DATABASE_URL);
+	if (!env.DATABASE_URL) {
+		throw new Error('DATABASE_URL tidak ditemukan');
+	}
 
-  if (!env.DATABASE_URL) {
-    throw new Error('DATABASE_URL tidak ditemukan');
-  }
-
-  return createClient({
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN
-  });
+	return createClient({
+		url: env.DATABASE_URL,
+		authToken: env.DATABASE_AUTH_TOKEN
+	});
 }
